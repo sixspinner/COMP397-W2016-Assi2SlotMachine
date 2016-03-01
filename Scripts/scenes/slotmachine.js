f9c2ccf72d2c4a0d884f54a44e6268cf018a6359
Sean Smith <srsmith92@gmail.com>
2016-02-29 21:23:34 -0500

    Initial commit; slightly modified version of class snapshot

---
+++ Scripts/scenes/slotmachine.js
@@ -0,0 +1,122 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+// MENU SCENE
+var scenes;
+(function (scenes) {
+    var SlotMachine = (function (_super) {
+        __extends(SlotMachine, _super);
+        // CONSTRUCTOR ++++++++++++++++++++++
+        function SlotMachine() {
+            _super.call(this);
+            this._grapes = 0;
+            this._bananas = 0;
+            this._oranges = 0;
+            this._cherries = 0;
+            this._bars = 0;
+            this._bells = 0;
+            this._sevens = 0;
+            this._blanks = 0;
+        }
+        // PUBLIC METHODS +++++++++++++++++++++
+        // Start Method
+        SlotMachine.prototype.start = function () {
+            // add background image to the scene
+            this._backgroundImage = new createjs.Bitmap(assets.getResult("SlotMachine"));
+            this.addChild(this._backgroundImage);
+            // add Bet1Button to the scene
+            this._bet1Button = new objects.Button("Bet1Button", 168, 382, false);
+            this.addChild(this._bet1Button);
+            this._bet1Button.on("click", this._bet1ButtonClick, this);
+            // add Bet10Button to the scene
+            this._bet10Button = new objects.Button("Bet10Button", 240, 382, false);
+            this.addChild(this._bet10Button);
+            this._bet10Button.on("click", this._bet10ButtonClick, this);
+            // add Bet100Button to the scene
+            this._bet100Button = new objects.Button("Bet100Button", 312, 382, false);
+            this.addChild(this._bet100Button);
+            this._bet100Button.on("click", this._bet100ButtonClick, this);
+            // add SpinButton to the scene
+            this._spinButton = new objects.Button("SpinButton", 402, 382, false);
+            this.addChild(this._spinButton);
+            this._spinButton.on("click", this._spinButtonClick, this);
+            // Setup Background
+            this._setupBackground("WhiteBackground");
+            // FadeIn
+            this._fadeIn(500);
+            // add this scene to the global stage container
+            stage.addChild(this);
+        };
+        // SLOT_MACHINE Scene updates here
+        SlotMachine.prototype.update = function () {
+        };
+        //PRIVATE METHODS
+        /* Utility function to check if a value falls within a range of bounds */
+        SlotMachine.prototype._checkRange = function (value, lowerBounds, upperBounds) {
+            return (value >= lowerBounds && value <= upperBounds) ? value : -1;
+        };
+        /* When this function is called it determines the betLine results.
+        e.g. Bar - Orange - Banana */
+        SlotMachine.prototype._reels = function () {
+            var betLine = [" ", " ", " "];
+            var outCome = [0, 0, 0];
+            for (var spin = 0; spin < 3; spin++) {
+                outCome[spin] = Math.floor((Math.random() * 65) + 1);
+                switch (outCome[spin]) {
+                    case this._checkRange(outCome[spin], 1, 27):
+                        betLine[spin] = "blank";
+                        this._blanks++;
+                        break;
+                    case this._checkRange(outCome[spin], 28, 37):
+                        betLine[spin] = "Grapes";
+                        this._grapes++;
+                        break;
+                    case this._checkRange(outCome[spin], 38, 46):
+                        betLine[spin] = "Banana";
+                        this._bananas++;
+                        break;
+                    case this._checkRange(outCome[spin], 47, 54):
+                        betLine[spin] = "Orange";
+                        this._oranges++;
+                        break;
+                    case this._checkRange(outCome[spin], 55, 59):
+                        betLine[spin] = "Cherry";
+                        this._cherries++;
+                        break;
+                    case this._checkRange(outCome[spin], 60, 62):
+                        betLine[spin] = "Bar";
+                        this._bars++;
+                        break;
+                    case this._checkRange(outCome[spin], 63, 64):
+                        betLine[spin] = "Bell";
+                        this._bells++;
+                        break;
+                    case this._checkRange(outCome[spin], 65, 65):
+                        betLine[spin] = "Seven";
+                        this._sevens++;
+                        break;
+                }
+            }
+            return betLine;
+        };
+        //EVENT HANDLERS ++++++++++++++++++++
+        SlotMachine.prototype._bet1ButtonClick = function (event) {
+            console.log("Bet 1 Credit");
+        };
+        SlotMachine.prototype._bet10ButtonClick = function (event) {
+            console.log("Bet 10 Credit");
+        };
+        SlotMachine.prototype._bet100ButtonClick = function (event) {
+            console.log("Bet 100 Credit");
+        };
+        SlotMachine.prototype._spinButtonClick = function (event) {
+            console.log("Spin those reels!");
+            console.log(this._reels());
+        };
+        return SlotMachine;
+    })(objects.Scene);
+    scenes.SlotMachine = SlotMachine;
+})(scenes || (scenes = {}));
+//# sourceMappingURL=slotmachine.js.map
